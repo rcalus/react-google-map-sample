ef39e60b7752e8383ade41946b1f7e745b64d83d
Ryan Calus <rcalus@vectorform.com>
2022-03-04 10:54:53 -0500

    update map width; get current location

--- src/components/MapContainerOpenStreet.tsx
+++ src/components/MapContainerOpenStreet.tsx
@@ -1,30 +1,39 @@
 import { useEffect } from "react";
-import L, { LeafletMouseEvent } from "leaflet"
+import L, { LatLngLiteral, LeafletMouseEvent } from "leaflet"
 import { Coordinates } from "./ShowPinpointCoordinates";
 
-const MapContainerOpenStreet = (props: { onMapClick: (coordinates: Coordinates) => void}) => {
-    
-    useEffect(() => {
+const MapContainerOpenStreet = (props: { onMapClick: (coordinates: Coordinates) => void }) => {
 
+    useEffect(() => {
         const map = L.map('map', {
-            zoom: 20,
-            center: [42.3314, -83.0458],
+            zoom: 20
         });
 
-        L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", { 
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const pos: LatLngLiteral = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+                    map.setView(pos);
+                })
+        }
+
+        L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
             attribution: "&copy; OSM contributors"
         }).addTo(map);
 
-        map.on('click', function(e: LeafletMouseEvent){
+        map.on('click', function (e: LeafletMouseEvent) {
             new L.Marker(e.latlng).addTo(map);
-            props.onMapClick({ lat: e.latlng.lat, long: e.latlng.lng});
+            props.onMapClick({ lat: e.latlng.lat, long: e.latlng.lng });
         });
 
         return () => {
             map.off();
             map.remove();
         }
-    }, []);
+    }, [navigator.geolocation]);
 
     return (
         <>
